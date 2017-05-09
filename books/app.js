9350d7d9fae748b025feaadfb81f38eb7a67c888
Randy Ferrer <randfer@gmail.com>
2017-05-09 17:45:11 -0400

    Books sample

--- books/app.js
+++ books/app.js
@@ -12,6 +12,53 @@ mongoose.connect(db);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', function(req, res) {
+  res.send('Happy to be here yo!!');
+});
+
+app.post('/book', function(req, res) {
+  var newBook = new Book();
+
+  newBook.title = req.body.title;
+  newBook.author = req.body.author;
+  newBook.category = req.body.category;
+
+  newBook.save(function(err, book) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(book);
+      res.send(book);
+    }
+  });
+});
+
+app.get('/books', function(req, res) {
+  Book.find({})
+    .exec(function(err, books) {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log(books);
+        res.json(books);
+      }
+    });
+});
+
+app.get('/books/:id', function(req, res){
+  Book.findOne({
+    _id: req.params.id
+  })
+  .exec(function(err, book) {
+    if (err) {
+      res.send(err);
+    } else{
+      console.log(book);
+      res.json(book);
+    }
+  })
+});
+
 app.listen(PORT, function() {
   console.log(`app listening on port ${PORT}`);
 });
